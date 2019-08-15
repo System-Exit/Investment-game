80fe5f3138c9dafe6bfbd7aa58a5e193e84c1098
Elwyn <s3629491@student.rmit.edu.au>
2019-08-15 14:33:54 +1000

    Models and init

---
+++ models.py
@@ -0,0 +1,17 @@
+from . import db
+from flask_login import UserMixin
+
+class User(db.Model):
+    """Model for user accounts."""
+
+    __tablename__ = 'USER'
+
+    userID = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(255), nullable=False, unique=False)
+    lastname = db.Column(db.String(255), nullable=False, unique=False)
+    email = db.Column(db.String(40), unique=True, nullable=False)
+    gender = db.Column(db.String(20), unique=False, nullable=False)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password = db.Column(db.String(200),, unique=False, nullable=False)
+    verified = db.Column(db.String(20), unique=False)
+    
