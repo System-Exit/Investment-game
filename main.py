0131ca2d1dbdd3c734c187f7cf37a127f953fc1b
Elwyn <s3629491@student.rmit.edu.au>
2019-08-15 15:17:47 +1000

    Added login logic

--- main.py
+++ main.py
@@ -2,12 +2,29 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from config import Config
 from registration import UserRegistrationForm
+from flask_login import current_user, login_user
+from models import User
 
+MyCloud = True
 
 # Load app with bootstrap
 app = Flask(__name__)
 app.config.from_object(Config)
 Bootstrap(app)
+
+if MyCloud:
+    HOST = "35.201.8.54"
+    USER = "root"
+    PASSWORD = "RMIT1234!!!"
+    DATABASE = "USER"
+else:
+    HOST = ""
+    USER = "root"
+    PASSWORD = "Bondi2Beach"
+    DATABASE = "Library"
+
+SQLALCHEMY_DATABASE_URI = "mysql://{}:{}@{}/{}".format(USER, PASSWORD, HOST, DATABASE)
+
 
 # Routing for each page
 # TODO: Change where routing is handled
@@ -40,6 +57,23 @@ def registration():
         return redirect(url_for('index'))
     # Render template
     return render_template('registration.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        # Redirect to index
+        return redirect(url_for('index'))
+    # Initialise login form
+    form = LoginForm()
+    # Validate and process form data
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        return redirect(url_for('index'))
+    # Render template
+    return render_template('login.html', form=form)
 
 # Run the app
 if __name__ == '__main__':
