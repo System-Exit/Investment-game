9ab46ce7a196bb858a6db282a08951650a67e323
rmit-s3661720-daniel-linford <s3661720@student.rmit.edu.au>
2019-08-24 16:00:10 +1000

    Changed cloud database config to connect to localhost
    Since we are changing how we connect to the database to doing it by a proxy instead, we have changed the config to connect to localhost.

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'PLACEHOLDERSECRETKEY'
     GDB_USERNAME = 'root'
     GDB_PASSWORD = 'RMIT1234!!!'
-    GDB_HOST = '35.189.52.142'
+    GDB_HOST = '127.0.0.1'
     GDB_DATABASE = 'Database'
     GDB_QUERY = ''
