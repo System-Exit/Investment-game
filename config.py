c400a858887c6bbeff644ca3a2af7282dd3f696f
rmit-s3661720-daniel-linford <s3661720@student.rmit.edu.au>
2019-09-19 15:07:08 +1000

    Updated config file to use environment variables

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ class Config(object):
     Configuration class for application.
 
     """
-    SECRET_KEY = 'PLACEHOLDERSECRETKEY'
-    GDB_USERNAME = 'root'
-    GDB_PASSWORD = 'RMIT1234!!!'
-    GDB_HOST = '127.0.0.1'
-    GDB_DATABASE = 'Database'
-    GDB_QUERY = ''
+    SECRET_KEY = os.getenv('SECRET_KEY') or 'PLACEHOLDERSECRETKEY'
+    GDB_USERNAME = os.getenv('GDB_USERNAME') or 'root'
+    GDB_PASSWORD = os.getenv('GDB_PASSWORD') or 'RMIT1234!!!'
+    GDB_HOST = os.getenv('GDB_HOST') or '127.0.0.1'
+    GDB_DATABASE = os.getenv('GDB_DATABASE') or 'Database'
+    GDB_QUERY = os.getenv('GDB_QUERY') or ''
