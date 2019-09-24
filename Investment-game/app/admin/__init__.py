8456309844188f7c2568b5d2dac68f13adb37e33
rmit-s3661720-daniel-linford <s3661720@student.rmit.edu.au>
2019-09-24 13:24:40 +1000

    Revert "Add files via upload"
    This reverts commit d66dd1b061486ad08776fb797a12890ae49e925d.

--- Investment-game/app/admin/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-# Initialise blueprints
-bp = Blueprint('admin', __name__)
-
-# Import routes
-# Since PEP8 dislikes mid file imports, warning has been supressed
-# TODO: Consider a means of importing routes without breaking PEP8
-from app.admin import routes  # nopep8
--- Investment-game/app/main/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-# Initialise blueprints
-bp = Blueprint('main', __name__)
-
-# Import routes
-# Since PEP8 dislikes mid file imports, warning has been supressed
-# TODO: Consider a means of importing routes without breaking PEP8
-from app.main import routes  # nopep8
