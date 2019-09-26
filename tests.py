1813158d8ec8801d3ac98e8429c922e08469e3cc
rmit-s3661720-daniel-linford <s3661720@student.rmit.edu.au>
2019-09-26 15:42:13 +1000

    Added verify user test

--- tests.py
+++ tests.py
@@ -1,9 +1,12 @@
 from gdb_api import GoogleDatabaseAPI
 from config import Config
 from models import Base, User, Share
+from argon2 import PasswordHasher
 import pytest
 import unittest
 import os
+import random
+import string
 
 
 class TestConfig(Config):
@@ -83,15 +86,11 @@ class TestGoogleDatabaseAPI(unittest.TestCase):
         testuserID = 1
         testusername = "U1"
         testemail = "E1"
-        testuser = User(userID=testuserID, firstname="F", lastname="L",
-                        email=testemail, dob="2000-01-01",
-                        username=testusername, userpass="P1", gender="O",
-                        verified=True, banned=False, balance=100)
-        assert testuser.userID == 1
+        testuser = self.generatetestuser(
+            userID=testuserID, email=testemail, username=testusername)
         # Add user to database directly
         with self.gdb.sessionmanager() as session:
             session.add(testuser)
-
         # Get all users
         users, num = self.gdb.getusers()
         # Assert that user is present in returned users list
@@ -111,3 +110,82 @@ class TestGoogleDatabaseAPI(unittest.TestCase):
         user = self.gdb.getuserbyemail(testemail)
         # Assert that returned user is correct
         assert user.userID == testuserID
+
+    def test_verifyuser(self):
+        # Create user
+        username = "TestUser"
+        userpass = "TestPass123"
+        wrongpass = "NotTestPass123"
+        testuser = User(username=username, userpass=userpass)
+        # Add user to database directly
+        with self.gdb.sessionmanager() as session:
+            session.add(testuser)
+        # Assert that user is verified with correct password
+        assert self.gdb.verifyuser(username, userpass) is True
+        # Assert that user is not verified with incorrect password
+        assert self.gdb.verifyuser(username, wrongpass) is False
+
+    def generatetestuser(self, userID=None, firstname=None, lastname=None,
+                         email=None, dob=None, gender=None, username=None,
+                         userpass=None, verified=None, banned=None,
+                         balance=None, hashpassword=True):
+        """
+        Helper method for creating a test user that with specified
+        attributes. If a user attribute is not defined, a random value will
+        be generated and assigned instead.
+
+        Args:
+            All attributes of User object, all default to none and will
+                be generated for the user unless assigned a value. Unless
+                one is specified, the userID will not be generated here.
+            hashpassword (bool): Whether or not to hash the user's password.
+                Defaults to True.
+        Returns:
+            A user object with the given and generated attributes.
+
+        """
+        # Generate values for each attribute if one is not assigned.
+        if not firstname:
+            firstname = ''.join(random.choices(string.ascii_lowercase, k=10))
+        if not lastname:
+            lastname = ''.join(random.choices(string.ascii_lowercase, k=10))
+        if not email:
+            part = ''.join(random.choices(string.ascii_lowercase, k=10))
+            domain = ''.join(random.choices(string.ascii_lowercase, k=10))
+            email = f"{part}@{domain}.com"
+        if not dob:
+            year = random.randint(1900, 2015)
+            month = random.randint(1, 12)
+            day = random.randint(1, 28)
+            dob = f"{year}-{month}-{day}"
+        if not gender:
+            gender = ''.join(random.choices(["M", "F", "O"], k=1))
+        if not username:
+            username = ''.join(random.choices(string.ascii_lowercase, k=15))
+        if not userpass:
+            userpass = ''.join(random.choices(
+                string.ascii_letters + string.digits, k=10))
+        if not verified:
+            verified = random.choices([True, False])[0]
+        if not banned:
+            banned = random.choices([True, False])[0]
+        if not balance:
+            balance = random.randint(1000, 10000000)
+        # If hash pass is set, hash the userpass
+        if hashpass:
+            userpass = PasswordHasher().hash(userpass)
+        # Create user
+        generateduser = User(
+            userID=userID,
+            firstname=firstname,
+            lastname=lastname,
+            email=email,
+            dob=dob,
+            username=username,
+            userpass=userpass,
+            gender=gender,
+            verified=verified,
+            banned=banned,
+            balance=balance)
+        # Return generated user
+        return generateduser
